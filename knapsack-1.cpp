b4be6074d4fe4f68f5e38968500db7f6d471fd45
Asik8 <motasembillah84@gmail.com>
2024-10-08 14:51:04 +0600

    Find value of maximum capacity using val and weight

--- knapsack-1.cpp
+++ knapsack-1.cpp
@@ -1,22 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-ll n;
-vector <ll> v(n),w(n);
-const int N=1e5+20;
-int dp[N];
+const ll N=1e5+20;
+ll dp[N];
 
-
+ll maxWeight(ll n, ll w, ll weight[], ll val[])
+{
+    if(n<0 || w==0) return 0;
+    if(dp[n] != -1) return dp[n];
+    if(weight[n]<= w)
+    {
+        ll op1 = maxWeight(n-1,w-weight[n],weight,val)+val[n];
+        ll op2 = maxWeight(n-1,w,weight,val);
+        return dp[n] = max(op1,op2); 
+    }
+    else return maxWeight(n-1,w,weight,val);
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    ll t;
-    cin >> t;
-    while (t--) {
-        cin >> n;
-        for (auto& x:v) cin >>x;       
-        for (auto& x:w) cin >>x;       
-    }
+    ll n,w;
+    cin>>n>>w;
+    ll val[n],weight[n];
+    for (int i=0;i<n;i++) cin >>val[i];       
+    for (int i=0;i<n;i++) cin >>weight[i];  
+    memset(dp,-1,sizeof(dp));
+    cout<<maxWeight(n-1,w,weight,val)<<endl;     
     return 0;
 }
